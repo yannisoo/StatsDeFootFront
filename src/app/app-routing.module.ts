735025e14b882049a7246530430eb5fb274cda89
ybattist <yannis.battiston@capgemini.fr>
2020-10-18 02:25:52 +0200

    ajout team info ajout match history modif comparative card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RechercheComparatifComponent } from './recherche-comparatif/recherche-comparatif.component';
-
+import { MatchHistoryComparativeComponent } from './match-history-comparative/match-history-comparative.component'
 const routes: Routes = [
-  { path: '', component: RechercheComparatifComponent }
+  { path: '', component: RechercheComparatifComponent },
+  { path: 'compare/:team1/:team2', component: MatchHistoryComparativeComponent}
 ];
 
 @NgModule({
