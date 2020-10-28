97ba92ab2189aefa512cd2465c940a758f78d97d
ybattist <yannis.battiston@capgemini.fr>
2020-10-28 01:04:48 +0100

    linted

--- src/app/components/home/home-page/home-page.component.ts
+++ src/app/components/home/home-page/home-page.component.ts
@@ -10,24 +10,24 @@ import { MatchService } from 'src/app/services/match.service';
 export class HomePageComponent implements OnInit {
 
   // date used for getting matches
-  date
+  date;
   // list of matches shown
-  matches
+  matches;
 
   constructor(private match: MatchService) { }
 
   ngOnInit(): void {
-    // refactor today's date to fit the API 
+    // refactor today's date to fit the API
     this.date = formatDate(new Date(), 'yyyy-MM-dd', 'en');
     // call API with the date get all matches by date
     this.match.getMatchesByDate(this.date).subscribe((response) => {
     // set data to list of matches shown
       this.matches = response.fixtures;
     // sort data by league_id
-      this.matches.sort(function (a,b){
+      this.matches.sort((a, b) => {
         return a.league_id - b.league_id;
-      })
-    })
+      });
+    });
   }
 
 }
